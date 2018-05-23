deeef9bcee3a0b83021c887d941a90b789dfba17
jawahar <jawahar@192.168.1.110>
2018-05-22 23:34:29 -0400

    NodeJs - Creating WebSocket

---
+++ NodeLearning/09.WebSockets/09.01_CreateWebSocket/ws.js
@@ -0,0 +1,9 @@
+/**
+ * http://usejsdoc.org/
+ */
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({port:3000});
+
+wss.on("connection", function(ws) {
+	ws.send("Welcome to Cyber Security Chat");
+});
